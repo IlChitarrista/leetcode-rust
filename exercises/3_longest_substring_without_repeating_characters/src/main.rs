48dc22f3a04ca6f5d7b324a23b568eddac9c4c04
IlChitarrista <mattiaformichetti@gmail.com>
2022-08-13 11:10:03 +0200

    Fixed Exercise 3

--- exercises/3_longest_substring_without_repeating_characters/src/main.rs
+++ exercises/3_longest_substring_without_repeating_characters/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let s = String::from("abcabcbb");
+    let s = String::from("aslkdfniawuhe wrhservòawnaròos eufòojfbwenr89abnp9wefnbdsaf n");
     println!("{}", solution(s));
+    let s = String::from("aslkdfniawuhe wrhservòawnaròos eufòojfbwenr89abnp9wefnbdsaf n");
+    println!("{}", best_solution(s));
 }
 
 fn solution(s: String) -> i32 {
@@ -34,6 +36,34 @@ fn solution(s: String) -> i32 {
             return max as i32;
         }
         idx += 1;
+    }
+    max as i32
+}
+
+fn best_solution(s: String) -> i32 {
+    use std::collections::HashMap;
+    let s = s.as_bytes();
+    let (mut _l_idx, mut r_idx) = (0, 0);
+    let (mut temp_max, mut max) = (0, 0);
+    let mut temp: HashMap<char, usize> = HashMap::new();
+    while r_idx < s.len() {
+        let curr_val = temp.get(&(s[r_idx] as char));
+        let curr = s[r_idx] as char;
+        if curr_val == None {
+            temp.insert(curr, r_idx);
+            temp_max += 1;
+            r_idx += 1;
+        } else {
+            temp.remove(&(s[_l_idx] as char));
+            _l_idx += 1;
+            if temp_max > max {
+                max = temp_max;
+            }
+            temp_max = temp.keys().len();
+          }
+    }
+    if temp_max > max {
+        max = temp_max;
     }
     max as i32
 }
